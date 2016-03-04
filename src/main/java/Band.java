0bfeaf069c8bfbe34efcc83f93442a7ddab4376f
Matt Rosanio <mmrosani@syr.edu>
2016-03-04 08:19:41 -0800

    Add spec and functionality to override equals method when called on members of Band class

--- src/main/java/Band.java
+++ src/main/java/Band.java
@@ -29,4 +29,14 @@ public class Band {
       return con.createQuery(sql).executeAndFetch(Band.class);
     }
   }
+
+  @Override
+  public boolean equals(Object otherBand) {
+    if(!(otherBand instanceof Band)) {
+      return false;
+    } else {
+      Band newBand = (Band) otherBand;
+      return newBand.getName().equals(name) && newBand.getMusic().equals(music);
+    }
+  }
 }
--- src/test/java/BandTest.java
+++ src/test/java/BandTest.java
@@ -9,4 +9,11 @@ public class BandTest {
   public void all_returnsEmptyAtFirst() {
     assertEquals(Band.all().size(), 0);
   }
+
+  @Test
+  public void equals_returnsTrueIfBandsHaveSameNameAndMusic() {
+    Band newBand = new Band("Matt and the Matties", "Electro Fusion Funk Powersoul");
+    Band newBand2 = new Band("Matt and the Matties", "Electro Fusion Funk Powersoul");
+    assertTrue(newBand.equals(newBand2));
+  }
 }
