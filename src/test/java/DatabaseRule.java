f260917cd2443b4fd3d5065c48469a05dbe645e5
Matt Rosanio <mmrosani@syr.edu>
2016-03-04 07:55:29 -0800

    Setup initial files and folders

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,16 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/band_venues_test", "Matt", "Mro13542");
+   }
+
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM tasks *;";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
+}
